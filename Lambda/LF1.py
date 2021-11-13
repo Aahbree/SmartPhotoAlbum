2d46bcf0846b95c44c90be91dbff6fd427af4abc
Aahbree <67740010+Aahbree@users.noreply.github.com>
2021-11-13 11:44:09 -0500

    Update LF1.py

--- Lambda/LF1.py
+++ Lambda/LF1.py
@@ -59,6 +59,7 @@ def detectRekognitionLabel(bucket,name):
 
 def lambda_handler(event, context):
     # TODO implement
+    print("Hello Swathi")
     print(event["Records"][0])
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     name = event["Records"][0]["s3"]["object"]["key"]
